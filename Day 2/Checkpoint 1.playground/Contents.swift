4724f4c97e4b22baa8a9b08ba2f5a003a59f93b6
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-16 14:30:03 +0200

    Add Day 2 Checkout 1

---
+++ Day 2/Checkpoint 1.playground/Contents.swift
@@ -0,0 +1,5 @@
+import Cocoa
+
+let temperatureInCelcius = 20.0
+let fahrenheit = temperatureInCelcius * 9 / 5 + 32
+print("Temperature in Celcius: \(temperatureInCelcius)°C, and in Fahrenheit: \(fahrenheit)°F")
