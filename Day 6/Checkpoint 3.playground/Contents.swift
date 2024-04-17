a44c35b64231c971b11d60ca7b3e4befdd314e8d
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-17 10:00:42 +0200

    Improve Checkpoint 3 code

--- Day 6/Checkpoint 3.playground/Contents.swift
+++ Day 6/Checkpoint 3.playground/Contents.swift
@@ -3,11 +3,11 @@ import Cocoa
 // The problem is called fizz buzz
 
 for i in 1...100 {
-    if i % 3 == 0 && i % 5 == 0 {
+    if i.isMultiple(of: 3) && i.isMultiple(of: 5) {
         print("FizzBuzz")
-    } else if i % 3 == 0 {
+    } else if i.isMultiple(of: 3) {
         print("Fizz")
-    } else if i % 5 == 0 {
+    } else if i.isMultiple(of: 5) {
         print("Buzz")
     } else {
         print(i)
