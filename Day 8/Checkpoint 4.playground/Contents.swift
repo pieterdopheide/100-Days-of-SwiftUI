41f99064ba1695450af5af640ec4855ba0cb5d63
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-19 10:51:06 +0200

    Improve Checkpoint 4 code

--- Day 8/Checkpoint 4.playground/Contents.swift
+++ Day 8/Checkpoint 4.playground/Contents.swift
@@ -1,32 +1,22 @@
 import Cocoa
 
 enum SqrtError: Error {
-    case OutOfBounds
-    case NoRoot
+    case outOfBounds
+    case noRoot
 }
 
 func squareRoot(_ number: Int) throws -> Int {
     if number < 1 || number > 10_000 {
-        throw SqrtError.OutOfBounds
+        throw SqrtError.outOfBounds
     }
     
-    if number == 0 || number == 1 {
-        return number
+    for i in 1...100 {
+        if i * i == number {
+            return i
+        }
     }
     
-    var i = 1
-    var result = 1
-    
-    while result <= number {
-        i += 1
-        result = i * i
-    }
-    
-    if (i - 1) * (i - 1) != number {
-        throw SqrtError.NoRoot
-    }
-    
-    return i - 1
+    throw SqrtError.noRoot
 }
 
 do {
