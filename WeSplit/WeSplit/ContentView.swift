a34fcec4fd9f030ebde7447219f2b24632b21c4f
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-23 11:16:28 +0200

    Complete the WeSplit challenges
    The challenges are:
    - Add a head to the third section, saying "Amount per person".
    - Add another section showing the total amount for the check - i.e.,
    the original amount plust tip value, without dividing by the number of
    people.
    - Change the tip percentage picker to show a new screen rather than
    using a segmented control, and give it a wider range of options -
    everything from 0% to 100%.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -14,15 +14,14 @@ struct ContentView: View {
     @State private var split = 0.0
     @FocusState private var amountIsFocussed: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentage)
-        
-        let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        
         return grandTotal / peopleCount
     }
     
@@ -44,14 +43,18 @@ struct ContentView: View {
                 
                 Section("How much do you want to tip?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                        ForEach(0..<101, id: \.self) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
-                Section {
+                Section("Total amount") {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                }
+                
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
                 }
             }
