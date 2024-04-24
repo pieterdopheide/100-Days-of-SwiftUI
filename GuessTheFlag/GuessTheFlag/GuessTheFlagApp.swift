326b7ea23d17954e68c0b35f0bab919a0b6531d9
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-24 14:36:29 +0200

    Add GuessTheFlag project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dopheide,Pieter on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
