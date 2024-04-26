85f6e9823b7f2ed4c93c387c36fe5b0a76788410
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-26 13:54:11 +0200

    Add BetterRest project

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dopheide,Pieter on 26/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
