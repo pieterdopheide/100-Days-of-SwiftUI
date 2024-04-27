66e296dc4ec68ef50658906aac59c3dd229937a4
Pieter Dopheide <pjmdopheide@gmail.com>
2024-04-27 14:13:00 +0200

    Add WordScramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Dopheide,Pieter on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
