eb27785213e490bf20b782a2cb27bc52d4ae6d49
Pieter Dopheide <pjmdopheide@gmail.com>
2024-05-01 11:56:51 +0200

    Complete iExpense challenges

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -15,21 +15,47 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
+                Section("Business") {
+                    ForEach(expenses.items.filter({ $0.type == "Business" })) { item in
+                        HStack {
+                            Image(systemName: item.type == "Personal" ? "person" : "suitcase")
                             
-                            Text(item.type)
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                
+                                Text(item.type)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                                .expenseStyle(for: item)
                         }
-                        
-                        Spacer()
-                        
-                        Text(item.amount, format: .currency(code: "EUR"))
                     }
+                    .onDelete(perform: removeBusinessExpense)
                 }
-                .onDelete(perform: removeItems)
+                
+                Section("Personal") {
+                    ForEach(expenses.items.filter({ $0.type == "Personal" })) { item in
+                        HStack {
+                            Image(systemName: item.type == "Personal" ? "person" : "suitcase")
+                            
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                
+                                Text(item.type)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "EUR"))
+                                .expenseStyle(for: item)
+                        }
+                    }
+                    .onDelete(perform: removePersonalExpense)
+                }
             }
             .navigationTitle("iExpense")
             .toolbar {
@@ -43,8 +69,42 @@ struct ContentView: View {
         }
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func removeBusinessExpense(at offsets: IndexSet) {
+        for offset in offsets {
+            if let index = expenses.items.firstIndex(where: { $0.id == expenses.items.filter({ $0.type == "Business" })[offset].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+    
+    func removePersonalExpense(at offsets: IndexSet) {
+        for offset in offsets {
+            if let index = expenses.items.firstIndex(where: { $0.id == expenses.items.filter({ $0.type == "Personal" })[offset].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+}
+
+struct ExpenseStyle: ViewModifier {
+    let expenseItem: ExpenseItem
+    
+    func body(content: Content) -> some View {
+        if expenseItem.amount <= 10 {
+            content.foregroundColor(.green)
+        } else if expenseItem.amount <= 100 {
+            content.foregroundColor(.orange)
+        } else {
+            content
+                .font(.headline)
+                .foregroundColor(.red)
+        }
+    }
+}
+
+extension View {
+    func expenseStyle(for expenseItem: ExpenseItem) -> some View {
+        modifier(ExpenseStyle(expenseItem: expenseItem))
     }
 }
 
