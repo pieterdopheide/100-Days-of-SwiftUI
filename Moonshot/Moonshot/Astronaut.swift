bd7bccd5e4399504ea4ecf938ecd7bde8e5392e9
Pieter Dopheide <pjmdopheide@gmail.com>
2024-05-03 11:53:48 +0200

    Add Moonshot project

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Dopheide,Pieter on 03/05/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Dopheide,Pieter on 03/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
