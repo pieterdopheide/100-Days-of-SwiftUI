2513227f5ed2a09e89cb77f9174f33805ed6cfd3
Pieter Dopheide <pjmdopheide@gmail.com>
2024-05-07 16:31:56 +0200

    Add CupcakeCorner project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Dopheide,Pieter on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
