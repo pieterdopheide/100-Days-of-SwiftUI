6777fc6540934a79bad9ce76c6e280c1ae15c2fe
Pieter Dopheide <pjmdopheide@gmail.com>
2024-05-31 11:37:53 +0200

    Add Instafilter project

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Dopheide,Pieter on 29/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
