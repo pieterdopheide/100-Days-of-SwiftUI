ce364f346fb447997f8713fdde42c4a7134f4e51
Pieter Dopheide <pjmdopheide@gmail.com>
2024-06-27 10:03:07 +0200

    Fix accessibility problems in old projects

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -26,10 +26,22 @@ struct ContentView: View {
     
     let questionsToAsk = 8
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes. Top stripe blue, middle stripe black, bottom stripe white.",
+        "France": "Flag with three vertical stripes. Left stripe blue, middle stripe white, right stripe red.",
+        "Germany": "Flag with three horizontal stripes. Top stripe black, middle stripe red, bottom stripe gold.",
+        "Ireland": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe orange.",
+        "Italy": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe red.",
+        "Nigeria": "Flag with three vertical stripes. Left stripe green, middle stripe white, right stripe green.",
+        "Poland": "Flag with two horizontal stripes. Top stripe white, bottom stripe red.",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red.",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background.",
+        "Ukraine": "Flag with two horizontal stripes. Top stripe blue, bottom stripe yellow.",
+        "US": "Flag with many red and white stripes, with white stars on a blue background in the top-left corner."
+    ]
+    
     var body: some View {
         ZStack {
-//            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-//                .ignoresSafeArea()
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)
@@ -66,6 +78,7 @@ struct ContentView: View {
                         } label: {
                             FlagImage(countryName: countries[number])
                         }
+                        .accessibilityLabel(labels[countries[number], default: "Unknown flag"])
                         .opacity(flagTapped != number ? opacity : 1)
                         .rotation3DEffect(
                             .degrees(flagTapped == number ? animationAmount : 0), axis: (x: 0, y: 1, z: 0)
