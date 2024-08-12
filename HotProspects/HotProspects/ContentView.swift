a61b04121f522aa501b134dc9a783264b21dfe6a
Pieter Dopheide <pjmdopheide@gmail.com>
2024-08-12 12:04:07 +0200

    Add HotProspects project

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Dopheide,Pieter on 12/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ProspectsView(filter: .none)
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            
+            ProspectsView(filter: .contacted)
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            
+            ProspectsView(filter: .uncontacted)
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
